85b9d63fd290a449804cf66b72d97280c7fee9dd
HogRider <98631205+HogRider19@users.noreply.github.com>
2022-06-21 20:17:13 +0300

    Start!

--- app/finder/apps.py
+++ app/finder/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SearchmainConfig(AppConfig):
+class FinderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'searchMain'
+    name = 'finder'
