ea8b0a36ef3561b7b91678a9eb0a777a392f2a18
HogRider <98631205+HogRider19@users.noreply.github.com>
2022-06-22 22:21:08 +0300

    Started analysis and searchVacancies!

--- app/finder/views.py
+++ app/finder/views.py
@@ -1,3 +1,4 @@
+from audioop import avg
 from django.shortcuts import redirect, render
 import requests
 import json
@@ -11,7 +12,37 @@ def analysis(request):
     if request.method == 'GET':
         return render(request, 'finder/analysis.html')
     else:
-        return render(request, 'finder/analysis.html')
+
+        params = {
+            'text': request.POST['profession'],
+            'per_page':'100',
+            'page':'0',
+            'only_with_salary':True,
+        }
+
+        vacancies = []
+        for index in range(1,3):
+            try:
+                params['page'] = str(index)
+                vacancies += list(map(
+                    lambda x: x['salary'],
+                    requests.get('https://api.hh.ru/vacancies', params=params).json()['items']
+                ))
+            except KeyError:
+                break
+
+        count = len(vacancies)
+        salary_from = [i['from'] for i in vacancies if i['from'] != None]
+        salary_to = [i['to'] for i in vacancies if i['to'] != None]
+        max_salary = max(salary_from + salary_to)
+        salary_min = min(salary_from + salary_to)
+        salary_avg = sum(salary_from + salary_to)/count
+
+        return render(
+            request,
+            'finder/analysis.html',
+            {'info':{'count':count, 'max':max_salary, 'min':salary_min, 'avg':salary_avg}}
+        )
 
 
 def searchvacancies(request):
@@ -21,31 +52,9 @@ def searchvacancies(request):
 
         params = {
             'text': request.POST['profession'],
-            'per_page':'20',
+            'per_page':'30',
         }
 
-        vacancies_json = json.loads(
-            requests.get('https://api.hh.ru/vacancies', params=params).text
-        )
+        vacancies = requests.get('https://api.hh.ru/vacancies', params=params).json()['items']
 
-        '''
-        vacancies = {
-            'id':,
-            'name' :,
-            'area':{
-                'id':,
-                'name':
-            },
-            'salary':{
-                'from':,
-                'to':,
-                'currenscy':,
-            },
-            'address':{
-                'city':,
-                ''
-            }
-        }
-        '''
-
-        return render(request, 'finder/searchvacancies.html', {'vacancies' : 1})
+        return render(request, 'finder/searchvacancies.html', {'vacancies' : vacancies})
